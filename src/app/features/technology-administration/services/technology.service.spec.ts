18d100cc66a7be08813474e2a0a85da1cb4c56b4
Henseler Patrick <patrick.henseler@stud.hslu.ch>
2024-02-11 22:12:47 +0100

    Created simple unit test

--- src/app/features/technology-administration/services/technology.service.spec.ts
+++ src/app/features/technology-administration/services/technology.service.spec.ts
@@ -1,16 +1,27 @@
-import { TestBed } from '@angular/core/testing';
-
 import { TechnologyService } from './technology.service';
+import {of} from "rxjs";
+import {Technology} from "../types/Technology";
+import {Category} from "../types/Category";
 
 describe('TechnologyService', () => {
-  let service: TechnologyService;
+
+  let httpClientSpy: { get: jasmine.Spy, post: jasmine.Spy };
+  let technologyService: TechnologyService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(TechnologyService);
+    httpClientSpy = jasmine.createSpyObj('HttpClient', ['get', 'post']);
+    technologyService = new TechnologyService(httpClientSpy as any);
   });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
+
+  it('should return technology', () => {
+    const expectedTechnology: Technology = {
+      id: 1, name: 'Angular', category: Category.LANGUAGES_AND_FRAMEWORKS,
+      description: 'A platform for building mobile and desktop web applications.', published: true
+    }
+
+    httpClientSpy.get.and.returnValue(of(expectedTechnology));
+    technologyService.getTechnology(1).subscribe(technology=> expect(technology).toEqual(expectedTechnology));
+    expect(httpClientSpy.get.calls.count()).toBe(1);
   });
 });
